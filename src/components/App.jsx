2144f9091eb9fe6c7012498deaf92e1740b8730c
Arpan Maity <33843098+binary-greens@users.noreply.github.com>
2021-06-05 19:20:43 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import "../../public/style.css";
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Note></Note>
+      <Note></Note>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript & Reac.JS</h1>
+      <hr />
+      <p>
+        This was an amazing bootcanp taken up by Sharya Sinha. We covered
+        everything from scratch including Javascript, React.JS
+      </p>
+    </div>
+  );
+}
+
+export default Note;
